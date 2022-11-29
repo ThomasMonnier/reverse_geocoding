8220272c879cdfc323ec0db77b5fad6644a34cc7
ThomasMonnier <thomas.monnier@papernest.com>
2022-11-29 17:15:44 +0100

    [UP] add password protection

--- app.py
+++ app.py
@@ -12,19 +12,21 @@ if __name__ == "__main__":
 
     st.write('Get the correct address according to the coordinates (latitude / longitude) by using the Reverse Geocoding API from MapBox.')
 
-    col_11, col_12, col_2 = st.columns(3)
-    latlng = [36.54085544, -4.619933495]
-    with col_11:
-        lat = st.text_input('Latitude: ', value="36.54085544")
+    password = st.text_input('Password: ', value="")
+    if password == st.secrets["PASSWORD"]:
+        col_11, col_12, col_2 = st.columns(3)
+        latlng = [36.54085544, -4.619933495]
+        with col_11:
+            lat = st.text_input('Latitude: ', value="36.54085544")
 
-    with col_12:
-        lng = st.text_input('Longitude: ', value="-4.619933495")
+        with col_12:
+            lng = st.text_input('Longitude: ', value="-4.619933495")
 
-    with col_2:
-        get = st.button('Get the address')
-        if get:
-            try:
-                g = geocoder.mapbox(latlng, method='reverse', key=st.secrets["MAPBOX_ACCESS_TOKEN"])
-                st.json(g.json)
-            except Exception as e:
-                st.error(e)
+        with col_2:
+            get = st.button('Get the address')
+            if get:
+                try:
+                    g = geocoder.mapbox(latlng, method='reverse', key=st.secrets["MAPBOX_ACCESS_TOKEN"])
+                    st.json(g.json)
+                except Exception as e:
+                    st.error(e)
